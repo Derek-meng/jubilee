138223d4729c5dbdc8b79f44725476f30c96fa71
Derekmeng <a0985265734@gmail.com>
2020-08-18 14:08:14 +0800

    test

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Jubilee\Auth\AuthProvider::class,
         /*
          * Application Service Providers...
          */
